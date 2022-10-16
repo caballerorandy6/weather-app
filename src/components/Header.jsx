477f5f334cf1fd8505ba125c6f5c800cb662103d
Randy Caballero <caballerorandy6@gmail.com>
2022-10-16 13:28:51 -0500

    added container mx-auto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <h1 className="text-5xl text-yellow-400 text-center font-bold uppercase p-10">
+      <h1 className="container mx-auto text-5xl text-yellow-400 text-center font-bold uppercase p-10">
         Weather App
       </h1>
     </>
